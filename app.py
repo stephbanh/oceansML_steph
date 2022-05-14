b852939bb22a677a09b9af0984bef0f826dcffe7
ennegineer <enne.face@gmail.com>
2022-05-14 11:24:05 -0700

    base flask app initialize

---
+++ app.py
@@ -0,0 +1,33 @@
+# import dependencies
+from flask import Flask, jsonify, render_template, redirect
+from flask_pymongo import PyMongo
+
+#################################################
+# Flask Setup
+#################################################
+
+# Create an app
+app = Flask(__name__)
+app._static_folder = 'static'
+
+#################################################
+# Database Setup
+#################################################
+# # Create connection variable
+# app.config["MONGO_URI"] = "mongodb://localhost:27017/oceans_db"
+# # conn = "mongodb://localhost:27017/oceans_db"
+# # Pass connection to the pymongo instance.
+# # client = pymongo.MongoClient(conn)
+# mongo = PyMongo(app)
+
+#################################################
+# Flask Routes
+#################################################
+@app.route("/")
+def home():
+    print("Server received request for 'Home' page...")
+
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run()
